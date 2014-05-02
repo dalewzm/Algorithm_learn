90ce59ec4adbff12138d4daba3de3e1428c74f9e
dalewzm <691077364@qq.com>
2014-05-02 14:53:36 +0800

    mwthod2

--- Merge Sorted Array.cpp
+++ Merge Sorted Array.cpp
@@ -1,3 +1,4 @@
+//Method1: AC
 //Use temp array,time complexity  is  O(2(m+n))=O(m+n)
 //space complexity is O(m+n)
 class Solution {
@@ -25,3 +26,36 @@ public:
 		delete []c;
 	}
 };
+
+
+//Method2: TLE
+//Don't use temp array, worst time  complexity is O(mn+m)=O(mn)
+//but space complexity is O(1)
+class Solution {
+public:
+	void merge(int A[], int m, int B[], int n) {
+		int i=0,j=0,count=0,k;
+		
+		while(i<m || j<n){
+			if( i<m && j<n){
+				if(A[count]<=B[j]){
+					count++;
+					i++;
+				}
+				else{
+					for(k=m-i; k>=0; --k)
+						A[count+k+1]=A[count+k];
+					A[count++]=B[j++];
+				}
+			}
+			else if(i<m){
+				continue;
+			}
+			else{
+				A[count++]=B[j++];
+			}
+		}
+		
+	
+	}
+};  

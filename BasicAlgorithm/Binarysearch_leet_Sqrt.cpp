32d055a2fa9d6716eb9363268936698b77218b2f
dalewzm <wangzmin@mail2.sysu.edu.cn>
2015-06-11 00:55:21 +0800

    Create Binarysearch_leet_Sqrt.cpp

---
+++ BasicAlgorithm/Binarysearch_leet_Sqrt.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+	bool isAns(long long r, long long x)
+	{
+		if (r*r<=x && (r + 1)*(r + 1)>x)
+			return true;
+		return false;
+	}
+	int mySqrt(int x) {
+		if (x <= 0)
+			return 0;
+			long tar = x;
+		long long  low = 0,high= x;
+		long long mid = low + (high - low) / 2;
+		while (!isAns(mid, tar))
+		{
+			if ((mid + 1)*(mid + 1) <= tar)
+			{
+				low = mid+1;
+			}
+			else{
+				high = mid-1;
+			}
+			mid = low + (high - low) / 2;
+		}
+		return mid;
+	}
+};

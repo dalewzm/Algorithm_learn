0e4d80ff6b4fdb3f712058ee464a25d2e4d1a47b
dalewzm <wangzmin@mail2.sysu.edu.cn>
2015-12-30 21:37:02 +0800

    get every digit and plus in reverse order

---
+++ Math/leetcode_reverseInteger.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+      int res = 0;
+       while(x){
+        if(res>0x7fffffff/10 || res<int(0x80000000)/10)
+          return 0;
+        res = 10*res + x%10;
+        x/=10;
+      }
+      return res;
+    }
+};

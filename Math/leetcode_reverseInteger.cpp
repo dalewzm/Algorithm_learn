e8c9cbd7b47556035f05ebaf63db59c92e4f449c
dalewzm <wangzmin@mail2.sysu.edu.cn>
2016-01-01 10:14:11 +0800

    Update leetcode_reverseInteger.cpp

--- Math/leetcode_reverseInteger.cpp
+++ Math/leetcode_reverseInteger.cpp
@@ -1,3 +1,15 @@
+//maybe better solution
+int reverse(int x) {
+    long long res = 0;
+     while(x){
+        res = res*10 + x%10;
+        x/=10;
+    }
+    return (res>0x7fffffff || res<-2147483648)?0:res;
+}
+
+
+
 class Solution {
 public:
     int reverse(int x) {

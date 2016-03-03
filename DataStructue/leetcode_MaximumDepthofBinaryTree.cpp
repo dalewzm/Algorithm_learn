d9cc07a7060a78954bc3a8fbf5f86e19d85944ef
dalewzm <691077364@qq.com>
2016-03-03 21:14:25 +0800

    Create leetcode_MaximumDepthofBinaryTree.cpp

---
+++ DataStructue/leetcode_MaximumDepthofBinaryTree.cpp
@@ -0,0 +1,11 @@
+ #include<algorithm>
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root)
+        {
+            return max(maxDepth(root->left),maxDepth(root->right))+1;
+        }
+        return 0;
+    }
+};

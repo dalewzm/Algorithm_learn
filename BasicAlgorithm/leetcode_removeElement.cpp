620a9dcfc896a322704aab1f6944d67dca65eb96
dalewzm <691077364@qq.com>
2016-03-30 16:21:46 +0800

    不实际删除元素的做法

--- BasicAlgorithm/leetcode_removeElement.cpp
+++ BasicAlgorithm/leetcode_removeElement.cpp
@@ -1,3 +1,22 @@
+//不删除元素，只是采取赋值，避免了删除导致的内存变化
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i = 0;
+        int n = nums.size();
+        while(i<n)
+        {
+            if(nums[i]==val){
+                nums[i] = nums[n-1];
+                n--;
+            }
+            else
+              i++;
+        }
+        return n;
+    }
+};
+
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {

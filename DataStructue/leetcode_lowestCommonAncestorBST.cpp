4fe5a7355988566e86b198756724c5fcd7509954
dalewzm <691077364@qq.com>
2016-04-02 16:16:40 +0800

    更简洁写法

--- DataStructue/leetcode_lowestCommonAncestorBST.cpp
+++ DataStructue/leetcode_lowestCommonAncestorBST.cpp
@@ -1,28 +1,23 @@
 //BST 每个节点代表一个范围,在BST中找序号最大的节点就公共节点,满足一个节点在其左区间,另一个在其右区间即可
+//根据节点的值，就可以判断查询节点和当前节点的位置关系
 class Solution {
 public:
-    TreeNode* lowest(TreeNode* root, int low, int high, int p,int q)
+    TreeNode* findLCA(TreeNode* root)
     {
-        int val = root->val;    
-        if(p<=val){
-            if(q<val)
-               return lowest(root->left, low, val,p,q);
-            else
-               return root;
-        }
-        else{
-              return lowest(root->right, val, high,p,q);
-        }
-    }
-    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         if(!root)
             return NULL;
-        if(!p)
-            return q;
-        if(!q)
-            return p;
-        if(q->val < p->val)
-            return lowest(root,(signed)0x80000000 ,0x7ffffff,q->val,p->val );
-        return lowest(root,(signed)0x80000000 ,0x7ffffff,p->val,q->val );
+        if(root->val<minv )
+            return findLCA(root->right);
+        if(root->val>maxv)
+            return findLCA(root->left);
+        return root;
     }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || root == p || root == q)
+            return root;
+        maxv = max(p->val, q->val);
+        minv = min(p->val, q->val);
+        return findLCA(root);
+    }
+    int maxv,minv;
 };

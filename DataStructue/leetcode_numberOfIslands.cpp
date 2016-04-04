3d7ba68fc6edce7ad0fed9eaa7e72fdd2c3396b0
dalewzm <691077364@qq.com>
2016-04-04 15:05:48 +0800

    增加并查集解法

--- DataStructue/leetcode_numberOfIslands.cpp
+++ DataStructue/leetcode_numberOfIslands.cpp
@@ -33,3 +33,91 @@ public:
         return cnt;
     }
 };
+
+//解法二: 并查集解法
+class Solution {
+public:
+	int  transPos21(int a, int b)
+	{
+		return dimy*a + b;
+	}
+	void init()
+	{
+		int pos = 0;
+		for (int i = 0; i < dimx; ++i)
+		{
+			for (int j = 0; j < dimy; ++j){
+				pos = transPos21(i, j);
+				pre[pos] = pos;
+				rank[pos] = 1;
+			}
+		}
+	}
+
+	void Union(int a, int b, int c, int d)
+	{
+		int pos1 = transPos21(a, b);
+		int pos2 = transPos21(c, d);
+		int x = find(pos1);
+		int y = find(pos2);
+		if (x != y){
+			if (rank[x] < rank[y])
+			{
+			    assert(pre[pos2]==y);
+				pre[x] = y;
+			}
+			else{
+				pre[y] = x;
+				if(rank[x]==rank[y])
+				    rank[x]++;
+			}
+		}
+	}
+	int find(int p)
+	{
+		if (pre[p] != p)
+			 pre[p] = find(pre[p]);
+		return pre[p];
+		//return pre[p] != p ? pre[p] = find(pre[p]): pre[p];
+	}
+
+	int numIslands(vector<vector<char>>& grid) {
+		pre.clear();
+		rank.clear();
+		if(grid.size() == 0)
+			return 0;
+		dimx = grid.size();
+		dimy = grid[0].size();
+		pre.resize(dimx * dimy);
+		rank.resize(dimx * dimy);
+		init();
+	   
+		for (int i = 0; i < dimx; ++i){
+			for (int j = 0; j < dimy; ++j){
+				if (grid[i][j] == '1'){
+					if (j + 1 < dimy && grid[i][j + 1] == '1')
+						Union(i, j, i, j + 1);
+					if (j - 1 >=0 && grid[i][j - 1] == '1')
+						Union(i, j, i, j - 1);
+					if (i - 1 >=0 && grid[i - 1][j] == '1')
+						Union(i, j, i - 1, j);
+					if (i + 1 < dimx && grid[i + 1][j] == '1')
+						Union(i, j, i + 1, j);
+				}
+			}
+		}
+		int cnt = 0;
+		for (int i = 0; i < dimx; ++i){
+			for (int j = 0; j < dimy; ++j){
+				int pos = transPos21(i, j);
+				if (grid[i][j]=='1' && find(pos) == pos)
+					cnt++;
+			}
+		}
+		return cnt;
+	}
+	vector<int> pre;
+	vector<int> rank;
+	int dimy;
+	int dimx;
+};

83a9af54a1cee482eb3fabc42ed4a28e9632d73b
dalewzm <691077364@qq.com>
2016-04-13 00:19:29 +0800

    更简洁的写法

--- Search/leetcode_PathSum.cpp
+++ Search/leetcode_PathSum.cpp
@@ -1,42 +1,17 @@
-//深搜遍历每条路径
+//更优雅的写法，每经过一个节点，就可以将要查询的值减去当前结点值，最后判断在叶子节点后，值是否为0
 class Solution {
 public:
-    void dfs(TreeNode* root, int &cur, int sum, bool &find)
-    {
+    bool hasPathSum(TreeNode *root, int sum) {
+        if(!root){
+            return false;
+        }
         if(!root->left && !root->right)
         {
-            cur += root->val;
-            if(cur == sum)
-              {
-                  find = true;
-                  return;
-              }
-             cur-=root->val;
-            return;
-        }
-        cur+= root->val;
-        
-        if(root->left)
-        {
-            dfs(root->left,cur,sum,find);
-        }
-        if(find)
-          return;
-        if(root->right)
-        {
-            dfs(root->right,cur,sum,find);
-        }
-        cur-= root->val;
-         
-    }
-    bool hasPathSum(TreeNode* root, int sum) {
-         bool find = false;
-         int cur = 0;
-         if(!root)
+            if(root->val == sum)
+                return true;
             return false;
-         dfs(root,cur,sum,find);
-         if(find)
-            return true;
-         return false;
+        }
+        return hasPathSum(root->left,sum-root->val)|| hasPathSum(root->right,
+                                                                sum-root->val);
     }
 };

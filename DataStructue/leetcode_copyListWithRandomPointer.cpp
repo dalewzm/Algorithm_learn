d6cf81a7cfc090966f9d810cd5018dca4b7542d0
dalewzm <691077364@qq.com>
2016-04-20 15:12:51 +0800

    增加额外空间复杂度O(1)解法

--- DataStructue/leetcode_copyListWithRandomPointer.cpp
+++ DataStructue/leetcode_copyListWithRandomPointer.cpp
@@ -1,3 +1,4 @@
+//解法一
 // 先不管random 指针，把整个链表复制一遍
 //同时用hash表记录原链表，每个节点指针对应的位置
 //另外一个哈希表记录复制链表位置所应得指针
@@ -45,4 +46,74 @@ public:
         
         return newHead;
     }
+};
+
+
+//解法二
+//不需要额外空间
+//1.先将原链表复制，每个节点复制一份作为自己的后继
+//例如：A->B  A->a->B->b
+//2.修改复制节点的random指针，即将复制节点的random 改为指向节点的next 节点
+//例如，假设上例B == A->random,那么复制后B == a->random, 不正确，应该B->next== b == a->random
+//3.将复制节点从链表中分离出来
+class Solution {
+public:
+	void copyToOneList(RandomListNode* head)
+	{
+		RandomListNode *nextNode = NULL;
+		while (head){
+			nextNode = head->next;
+			RandomListNode *cpNode = new RandomListNode(head->label);
+			cpNode->next = head->next;
+			cpNode->random = head->random;
+			head->next = cpNode;
+			head = nextNode;
+
+		}
+	}
+
+	void correctRandomPointer(RandomListNode* head)
+	{
+		RandomListNode *cpNode = NULL;
+		RandomListNode *randomNode = NULL;
+		while (head){
+			cpNode = head->next;
+			if (cpNode->random)
+			{
+				randomNode = cpNode->random;
+				cpNode->random = randomNode->next;
+			}
+			head = cpNode->next;
+		}
+
+	}
+	RandomListNode* seprateList(RandomListNode* head)
+	{
+		RandomListNode* cpHead = NULL,*cpEnd = NULL;
+		RandomListNode* cpNode = NULL;
+		int cnt = 0;
+		while (head){
+			cpNode = head->next;
+			if (cnt  == 0)
+			{
+				cpHead = cpNode;
+				cpEnd = cpHead;
+				cnt++;
+			}
+			else{
+				cpEnd->next = cpNode;
+				cpEnd = cpEnd->next;
+			}
+			head->next = cpNode->next;
+			head = cpNode->next;
+		}
+		return cpHead;
+
+	}
+
+	RandomListNode *copyRandomList(RandomListNode *head) {
+		copyToOneList(head);
+		correctRandomPointer(head);
+		return seprateList(head);
+	}
 };

7d5f09a6332011769600a1d51200057bdab17e9f
dalewzm <691077364@qq.com>
2016-04-24 21:12:11 +0800

    更好解法，高精度+1

--- Math/leetcode_plusOne.cpp
+++ Math/leetcode_plusOne.cpp
@@ -1,27 +1,24 @@
 //高精度加法简易模拟版
+
+//更高效解法，不需要翻转两次，如果需要增加位数，必定是999这种形式，结果是1000,首位置1，后面补0即可
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& digits) {
-        vector<int> ret(digits.begin(),digits.end());
-        if(digits.size()==0)
-            return ret;
-        reverse(ret.begin(),ret.end());
-        int add = 1;
-        for(int i=0;add && i<ret.size(); ++i)
-        {
-            ret[i]++;
-            if(ret[i]>9)
-            {
-                ret[i] = 0;
-                add = 1;
-            }
-            else{
-                add = 0;
-            }
-        }
-        if(add)
-        ret.push_back(1);
-        reverse(ret.begin(),ret.end());
-        return ret;
-    }
+	vector<int> plusOne(vector<int>& digits) {
+		vector<int> ret(digits);
+		int n = digits.size();
+		for (int i = n - 1; i >= 0; --i)
+		{
+			if (ret[i] == 9)
+			{
+				ret[i] = 0;
+			}
+			else{
+			    ret[i]++;
+				return ret;
+			}
+		}
+		ret[0] = 1;
+		ret.push_back(0);
+		return ret;
+	}
 };
